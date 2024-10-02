e12f8e4eb637ae3253a4ae5e736e5979ec51013c
noogen <tom@noogen.net>
2024-10-02 12:21:27 -0500

    update config location

--- reset.sh
+++ reset.sh
@@ -6,9 +6,8 @@ rm -rf data/prom/*
 rm -rf data/vector/*
 
 cp -n .env-example .env
+cp -n stat-transform.vrl data/stat-transform.vrl
 cp -n vector-example.yaml data/vector.yaml
 cp -n prometheus-example.yaml data/prometheus.yaml
 cp -n web-example.yaml data/web.yaml
-
-
 
