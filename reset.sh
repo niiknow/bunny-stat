8a33f37eadb0672894abeda3281e00afed39323e
noogen <tom@noogen.net>
2024-10-02 12:25:27 -0500

    update reset

--- reset.sh
+++ reset.sh
@@ -5,9 +5,10 @@ rm -rf data/openob/*
 rm -rf data/prom/*
 rm -rf data/vector/*
 
-cp -n .env-example .env
+cp -n .env-example data/.env
 cp -n stat-transform.vrl data/stat-transform.vrl
 cp -n vector-example.yaml data/vector.yaml
 cp -n prometheus-example.yaml data/prometheus.yaml
 cp -n web-example.yaml data/web.yaml
-
+cp -n web-example.yaml data/web.yaml
+cp -n docker-compose-example.yaml data/docker-compose.yaml
