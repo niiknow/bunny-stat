55854674a55ac58ff0ab2c16424953546786237e
noogen <tom@noogen.net>
2024-10-02 12:16:53 -0500

    move config files

--- reset.sh
+++ reset.sh
@@ -6,9 +6,9 @@ rm -rf data/prom/*
 rm -rf data/vector/*
 
 cp -n .env-example .env
-cp -n vector-example.yaml vector.yaml
-cp -n prometheus-example.yaml prometheus.yaml
-cp -n web-example.yaml web.yaml
+cp -n vector-example.yaml data/vector.yaml
+cp -n prometheus-example.yaml data/prometheus.yaml
+cp -n web-example.yaml data/web.yaml
 
 
 
